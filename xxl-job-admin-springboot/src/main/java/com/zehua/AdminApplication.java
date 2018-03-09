455eec25b5b3c4b6f9f1b91d556a881bf65ca452
zehua <523786283@qq.com>
2018-03-09 23:06:43 +0800

    添加spring boot版的调度中心

---
+++ xxl-job-admin-springboot/src/main/java/com/zehua/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.zehua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linzehua
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
